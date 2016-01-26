9f4f10acc8dec17a82bd139a43ae44ec7094a240
Devontrae Walls <contact@devontrae.com>
2016-01-26 06:55:48 -0800

    Update memberBox_sessions.sql

--- memberBox_sessions.sql
+++ memberBox_sessions.sql
@@ -1,11 +1,11 @@
-        CREATE TABLE `memberBox_sessions` (
-          `session_id` int(11) NOT NULL AUTO_INCREMENT,
-          `session_token` varchar(255) NOT NULL,
-          `session_user` int(3) NOT NULL,
-          `session_ip` varchar(20) NOT NULL,
-          `session_useragent` text NOT NULL,
-          `session_admin` int(1) NOT NULL,
-          `session_time_initiated` int(20) NOT NULL,
-          `session_time_expiration` int(20) NOT NULL,
-          PRIMARY KEY (`session_id`)
-        ) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
+CREATE TABLE `memberBox_sessions` (
+  `session_id` int(11) NOT NULL AUTO_INCREMENT,
+  `session_token` varchar(255) NOT NULL,
+  `session_user` int(3) NOT NULL,
+  `session_ip` varchar(20) NOT NULL,
+  `session_useragent` text NOT NULL,
+  `session_admin` int(1) NOT NULL,
+  `session_time_initiated` int(20) NOT NULL,
+  `session_time_expiration` int(20) NOT NULL,
+  PRIMARY KEY (`session_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1;
